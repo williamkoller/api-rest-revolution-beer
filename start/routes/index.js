cc37b393e79aca9028494620272b8e763e8155d5
William Koller <wkoller25@gmail.com>
2020-10-16 00:34:37 -0300

    feat: add require routes

---
+++ start/routes/index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+  String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10) // don't forget the second param
+    var hours = Math.floor(sec_num / 3600)
+    var minutes = Math.floor((sec_num - hours * 3600) / 60)
+    var seconds = sec_num - hours * 3600 - minutes * 60
+
+    if (hours < 10) {
+      hours = '0' + hours
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds
+    }
+    var time = hours + ':' + minutes + ':' + seconds
+    return time
+  }
+
+  var time = process.uptime()
+  var uptime = (time + '').toHHMMSS()
+
+  return {
+    greeting: 'Wellcome to API Rest Revolution Beer',
+    version: process.version,
+    uptime: uptime
+  }
+})
+
+require('./user')
